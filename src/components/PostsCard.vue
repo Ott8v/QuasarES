d8ad534c7f1f0db27a99b3da2a4d4088d28e9507
Federico Balducci <ott8v@proton.me>
2024-02-14 11:26:23 +0100

    fix popup

--- src/components/PostsCard.vue
+++ src/components/PostsCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card class="my-card">
+  <q-card class="my-card" @click="() => setStatus()">
     <q-card-section>
       <div class="text-h6">{{ post.title }}</div>
       <div class="text-subtitle2">by User: {{ post.userId }}</div>
--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card class="my-card" @click="() => setStatus()">
+  <q-card class="my-card">
     <q-card-section>
       <div class="text-h6">{{ obj.title }}</div>
       <div class="text-subtitle2">by User: {{ obj.userId }}</div>
